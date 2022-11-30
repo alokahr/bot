d0f5216f267ac5c84ff64cc653eafca791fd5eb0
Alok Ahirwar <69086774+alokahr@users.noreply.github.com>
2022-11-30 11:30:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ class DefaultConfig:
 
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "aa50a6bb-a6c1-48e5-8f68-ed925e13c538")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Ph48Q~epOi3UsJsQqkaHW0rcbhM-MpGm6.qrZaM5")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Bi08Q~9PLNObF2EOWgdWtqXunrsMLnopeUpAqcnd")
